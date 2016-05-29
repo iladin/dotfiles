b56e7aa43fb91b49b77ea2e64198e5440292b51d
iladin <iladin@gmail.com>
2016-05-29 08:23:36 +0000

    Fixed error in awk invocation
    Fun story: a responder posted here
    https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotfiles-git-bare-repo/#comment-2585197270
    that there was a "typo" but didn't say what it was.

--- cfg.sh
+++ cfg.sh
@@ -9,7 +9,7 @@ if [[ ! -d $HOME/.cfg ]]; then
    echo "Checked out config.";
    else
      echo "Backing up pre-existing dot files.";
-     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+     config checkout 2>&1 | egrep "\s+\." | awk '$1=$1' | xargs -I{} mv {} .config-backup/{}
  fi;
  config checkout
  config config status.showUntrackedFiles no
