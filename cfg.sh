9865c9974ad47e3ae9c92ed04f1759e179e4fd50
iladin <iladin@gmail.com>
2016-05-29 15:59:19 +0000

    Rename config function cfg
    It's half the  letters!

--- cfg.sh
+++ cfg.sh
@@ -1,18 +1,18 @@
-config () {
+cfg () {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 if [[ ! -d $HOME/.cfg ]]; then
  git clone --bare https://github.com/iladin/dotty.git   $HOME/.cfg
  mkdir -p .config-backup
- config checkout
+ cfg checkout
  if [ $? = 0 ]; then
    echo "Checked out config.";
    else
      echo "Backing up pre-existing dot files.";
-     config checkout 2>&1 | egrep "\s+\." | awk '$1=$1' | while read -r file; do
+     cfg checkout 2>&1 | egrep "\s+\." | awk '$1=$1' | while read -r file; do
      test -L "$file" && unlink "$file" || mv "$HOME/.config-backup/$file"
    done
  fi;
- config checkout
- config config status.showUntrackedFiles no
+ cfg checkout
+ cfg config status.showUntrackedFiles no
  fi
