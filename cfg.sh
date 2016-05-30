784ef69e705ab7fa11a447985c3924febde84c90
iladin <iladin@gmail.com>
2016-05-30 02:44:58 +0000

    Make config-backup in $HOME
    Also renamed config-backup to cfg-backup
    Just to confuse you later

--- cfg.sh
+++ cfg.sh
@@ -4,14 +4,14 @@ cfg () {
 }
 if [[ ! -d $HOME/.cfg ]]; then
  git clone --bare https://github.com/iladin/dotty.git   $HOME/.cfg
- mkdir -p .config-backup
+ mkdir -p "$HOME/.cfg-backup"
  cfg checkout
  if [ $? = 0 ]; then
    echo "Checked out config.";
    else
      echo "Backing up pre-existing dot files.";
      cfg checkout 2>&1 | egrep "\s+\." | awk '$1=$1' | while read -r file; do
-     test -L "$HOME/$file" && unlink "$HOME/$file" || mv "$HOME/$file" "$HOME/.config-backup/$file"
+     test -L "$HOME/$file" && unlink "$HOME/$file" || mv "$HOME/$file" "$HOME/.cfg-backup/$file"
    done
  fi;
  cfg checkout
