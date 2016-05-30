cf94669d5b361b7c4995408ec67f078c0e709c9e
iladin <iladin@gmail.com>
2016-05-29 22:11:04 -0700

    Create makeZSH.sh
    install zsh from source into hidden .root directory

---
+++ make/makeZSH.sh
@@ -0,0 +1,32 @@
+git clone git://git.code.sf.net/p/zsh/code
+
+export CHROOT=$HOME/.root
+mkdir -p $CHROOT
+cd zsh
+
+# Options from Ubuntu Zsh package rules file (http://launchpad.net/ubuntu/+source/zsh)
+./configure --prefix=$CHROOT \
+            --mandir=$CHROOT/man \
+            --bindir=$CHROOT/bin \
+            --infodir=$CHROOT/usr/share/info \
+            --enable-maildir-support \
+            --enable-max-jobtable-size=256 \
+            --enable-etcdir=$CHROOT/etc/zsh \
+            --enable-function-subdirs \
+            --enable-site-fndir=$CHROOT/usr/local/share/zsh/site-functions \
+            --enable-fndir=$CHROOT/usr/share/zsh/functions \
+            --with-tcsetpgrp \
+            --with-term-lib="ncursesw" \
+            --enable-cap \
+            --enable-pcre \
+            --enable-readnullcmd=pager \
+            --enable-custom-patchlevel=Debian \
+            LDFLAGS="-Wl,--as-needed -g"
+
+make
+
+make check
+
+make install
+
+make install.info
