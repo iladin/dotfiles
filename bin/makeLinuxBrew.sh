aba4436bb6e6af98a523abcfe2584246a496fbbd
Daniel Silverman <iladin@gmail.com>
2016-06-01 20:07:06 -0700

    Better path management

--- bin/makeLinuxBrew.sh
+++ bin/makeLinuxBrew.sh
@@ -1,4 +1,10 @@
-PATH=~/.linuxbrew/bin:/usr/local/bin:/usr/bin:/bin
+# TODO: test if pathadd is defined
+
+pathadd ~/.linuxbrew/bin
+pathadd ~/.linuxbrew/sbin
+
+#TODO: give pathadd a for loop
+
 unset LD_LIBRARY_PATH PKG_CONFIG_PATH
 yes | ruby -e "$(curl -fsSL https://raw.github.com/Linuxbrew/linuxbrew/go/install)"
 brew doctor
@@ -8,5 +14,4 @@ brew tap homebrew/dupes
 brew install git
 brew install coreutils findutils gawk gnu-sed gnu-which grep make ruby
 
-PATH=$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin
 brew install hello && brew test hello; brew remove hello
