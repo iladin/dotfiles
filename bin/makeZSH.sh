2876e917faa42bac41d8e9496869cbadbb3e1597
Daniel Silverman <iladin@gmail.com>
2016-06-03 01:50:10 -0700

    Home is root

--- bin/makeZSH.sh
+++ bin/makeZSH.sh
@@ -1,21 +1,21 @@
 set -xv
 git clone git://git.code.sf.net/p/zsh/code zsh
 
-export CHROOT=$HOME
+$HOME
 cd zsh
 
 autoconf
 
 # Options from Ubuntu Zsh package rules file (http://launchpad.net/ubuntu/+source/zsh)
-./configure --prefix=$CHROOT \
-            --mandir=$CHROOT/man \
-            --bindir=$CHROOT/bin \
-            --infodir=$CHROOT/usr/share/info \
+./configure --prefix=$HOME \
+            --mandir=$HOME/man \
+            --bindir=$HOME/bin \
+            --infodir=$HOME/usr/share/info \
             --enable-maildir-support \
-            --enable-etcdir=$CHROOT/etc/zsh \
+            --enable-etcdir=$HOME/etc/zsh \
             --enable-function-subdirs \
-            --enable-site-fndir=$CHROOT/usr/local/share/zsh/site-functions \
-            --enable-fndir=$CHROOT/usr/share/zsh/functions \
+            --enable-site-fndir=$HOME/usr/local/share/zsh/site-functions \
+            --enable-fndir=$HOME/usr/share/zsh/functions \
             --with-tcsetpgrp \
             --with-term-lib="ncursesw" \
             --enable-cap \
