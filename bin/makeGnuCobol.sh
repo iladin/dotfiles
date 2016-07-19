3cfadb2f6393144620a5bfd0ac5e5bf658c91633
Daniel Silverman <iladin@gmail.com>
2016-07-18 21:13:59 -0700

    updated gnucobol

--- bin/makeGnuCobol.sh
+++ bin/makeGnuCobol.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+mkdir ~/tmp 2> /dev/null
+cd ~/tmp
+wget https://sourceforge.net/code-snapshots/svn/o/op/open-cobol/code/open-cobol-code-999-trunk.zip
+unzip open-cobol-code-999-trunk.zip
+cd open-cobol-code-999-trunk/gnu-cobol
+sudo apt-get install libdb-dev
+./configure
+make
+sudo make install
+
