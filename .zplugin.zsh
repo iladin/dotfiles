e0fa59fe1a59150d62f8b5cb51ed10975acc0a15
Daniel Silverman <iladin@gmail.com>
2016-07-18 19:25:27 -0700

    Changed fzf test conditions

--- .zplugin.zsh
+++ .zplugin.zsh
@@ -33,7 +33,7 @@ zplugin load "mollifier/anyframe"
 hash python && zplugin load "djui/alias-tips"
 
 # fzf is a general-purpose command-line fuzzy finder.
-hash fzf 2> /dev/null || (git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf ; ~/.fzf/install --all)
+[ -f ~/.fzf.zsh ] || (git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf ; ~/.fzf/install --all)
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 # A next-generation cd command with an interactive filter
 zplugin load "b4b4r07/enhancd" #, of:enhancd.sh
