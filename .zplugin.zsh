fb4176d2e35a8c888def52bb36f407ca10064078
Daniel Silverman <iladin@gmail.com>
2016-07-20 21:24:00 -0700

    Different git zsh plugins

--- .zplugin.zsh
+++ .zplugin.zsh
@@ -44,8 +44,9 @@ zplugin load "joshuarubin/zsh-homebrew"
 
 hash docker &> /dev/null && zplugin load  "felixr/docker-zsh-completion"
 
-zplugin snippet 'http://github.com/robbyrussell/oh-my-zsh/raw/master/lib/git.zsh'
-zplugin snippet "https://github.com/sorin-ionescu/prezto/blob/master/modules/git/alias.zsh"
+zplugin snippet https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/git/git.plugin.zsh
+zplugin snippet https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/git.zsh
+
 zplugin load "mrowa44/emojify" #, as:command, of:emojify
 #Incremental history word completing (started with Alt-h/H or Option-h/H on Mac)
 zplugin load "psprint/zsh-editing-workbench"
