33f0221c9f8fb28f524b84756e72861e8b0c8ac7
Daniel Silverman <iladin@gmail.com>
2016-08-08 06:11:54 -0700

    Updated cfg.sh to zsh version

--- cfg.sh
+++ cfg.sh
@@ -1,20 +1,6 @@
-cfg () {
-   git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
-}
-if [[ ! -d $HOME/.cfg ]]; then
- git clone --bare $(git config --get remote.origin.url)  $HOME/.cfg
- mkdir -p "$HOME/.cfg-backup"
-fi
- cfg checkout
- if [ $? = 0 ]; then
-   echo "Checked out config.";
-   else
-     echo "Backing up pre-existing dot files.";
-     cfg checkout 2>&1 | sed 's/^M//g' | egrep "^[[:space:]]" | awk '$1=$1' |\
-         while read -r file
-         do
-              mv "$HOME/$file" "$HOME/.cfg-backup/$file" || unlink "$HOME/$file"
-         done
- fi;
- cfg checkout --force
- cfg config status.showUntrackedFiles no
+cfg () {   git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@ }
+test -d $HOME/.cfg || (git clone --bare https://gitlab.com/iladin/dotfiles.git  $HOME/.cfg; mkdir -p "$HOME/.cfg-backup")
+cfg checkout 2> /dev/null || cfg checkout 2>&1 | sed 's/^M//g' | egrep "^[[:space:]]" | awk '$1=$1' |\
+    while read -r file; do mv "$HOME/$file" "$HOME/.cfg-backup/$file" 2> /dev/null || unlink "$HOME/$file" 2> /dev/null ;done
+cfg checkout --force
+cfg config status.showUntrackedFiles no
