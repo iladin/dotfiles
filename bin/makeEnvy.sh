b7776faf0e7751e515767a3550a738e883243db4
Daniel Silverman <iladin@gmail.com>
2016-08-11 22:34:57 -0700

    Added function to locate dockerfile
    Prefer current directory over home

--- bin/makeEnvy.sh
+++ bin/makeEnvy.sh
@@ -1,5 +1,18 @@
+
+function whereMyDockerfileAt(){
+    if [[ -e .codenvy.dockerfile ]]; then
+        dockerfile=.codenvy.dockerfile
+    elif [[ -e ~/.codenvy.dockerfile ]]; then
+        dockerfile=~/.codenvy.dockerfile
+    else
+        echo "Cannot find dockerfile, exiting"
+        exit 1
+    fi
+
+}
+whereMyDockerfileAt
 if [[ $UID != 0 ]]; then
-    sudo bash -c "eval $(sed -n '/RUN/,/^$/p' ~/.codenvy.dockerfile | sed 's/RUN//')"
+    sudo bash -c "eval $(sed -n '/RUN/,/^$/p' $dockerfile | sed 's/RUN//')"
 else
-    bash -c "eval $(sed -n '/RUN/,/^$/p' ~/.codenvy.dockerfile | sed 's/RUN//')"
+    bash -c "eval $(sed -n '/RUN/,/^$/p' $dockerfile | sed 's/RUN//')"
 fi
