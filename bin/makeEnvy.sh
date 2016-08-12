415c441e0039179c9a02ec5447e7bde92d89f5c2
Daniel Silverman <iladin@gmail.com>
2016-08-11 22:13:02 -0700

    Updated makeEnvy to work as root

--- bin/makeEnvy.sh
+++ bin/makeEnvy.sh
@@ -1,1 +1,5 @@
-sudo bash -c "eval $(sed -n '/RUN/,/^$/p' ~/.codenvy.dockerfile | sed 's/RUN//')"
+if [[ $UID != 0]]; then
+    sudo bash -c "eval $(sed -n '/RUN/,/^$/p' ~/.codenvy.dockerfile | sed 's/RUN//')"
+else
+    bash -c "eval $(sed -n '/RUN/,/^$/p' ~/.codenvy.dockerfile | sed 's/RUN//')"
+fi
