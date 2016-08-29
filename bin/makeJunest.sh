5f466c6fed56d01cb366bb465983d44be3463a5c
Daniel Silverman <iladin@gmail.com>
2016-08-29 16:12:44 -0700

    Added grep and awk to junest

--- bin/makeJunest.sh
+++ bin/makeJunest.sh
@@ -1,3 +1,3 @@
 git clone git://github.com/fsquillace/junest ~/junest
 export PATH=~/junest/bin:$PATH
-junest -f -- pacman --noconfirm -Sy fish zsh
+junest -f -- pacman --noconfirm -Sy zsh awk pkgfile grep
