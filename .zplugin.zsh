4762e3cca11642c15d4c90ef76de9d6b5ba8a1e3
Daniel Silverman <iladin@gmail.com>
2016-08-29 21:10:30 -0700

    Try using $zp_cmd so I can switch later...
    Says the guy who should be in a branch

--- .zplugin.zsh
+++ .zplugin.zsh
@@ -31,61 +31,61 @@ zzplugin
 autoload -Uz compinit
 compinit
 
-zplugin load "iladin/zstyle"
+$zp_cmd "iladin/zstyle"
 
 # k is a zsh script / plugin to make directory listings more readable
 #adding a bit of color and some git status information on files and directories.
-zplugin load "supercrabtree/k"
+$zp_cmd "supercrabtree/k"
 # A fully-functional bash client for tldr.
-zplugin load "raylee/tldr" #, as:command
+$zp_cmd "raylee/tldr" #, as:command
 # peco/percol/fzf wrapper plugin for zsh
-zplugin load "mollifier/anyframe"
+$zp_cmd "mollifier/anyframe"
 # An oh-my-zsh plugin to help remembering those aliases you defined once
 # Only use if you have python
-hash python && zplugin load "djui/alias-tips"
+hash python && $zp_cmd "djui/alias-tips"
 
 
 # A next-generation cd command with an interactive filter
-zplugin load "b4b4r07/enhancd" #, of:enhancd.sh
+$zp_cmd "b4b4r07/enhancd" #, of:enhancd.sh
 #Emoji completion on the command line
-zplugin load "b4b4r07/emoji-cli"
+$zp_cmd "b4b4r07/emoji-cli"
 
-zplugin load "joshuarubin/zsh-homebrew"
+$zp_cmd "joshuarubin/zsh-homebrew"
 
-hash docker &> /dev/null && zplugin load  "felixr/docker-zsh-completion"
+hash docker &> /dev/null && $zp_cmd  "felixr/docker-zsh-completion"
 
 zplugin snippet 'https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/git/git.plugin.zsh'
 zplugin snippet 'https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/git.zsh'
 zplugin snippet 'https://raw.githubusercontent.com/mchav/with/master/with'
-zplugin load "mrowa44/emojify" #, as:command, of:emojify
+$zp_cmd "mrowa44/emojify" #, as:command, of:emojify
 #Incremental history word completing (started with Alt-h/H or Option-h/H on Mac)
-zplugin load "psprint/zsh-editing-workbench"
+$zp_cmd "psprint/zsh-editing-workbench"
 
 #Because your terminal should be able to perform tasks asynchronously without external tools!
-zplugin load "mafredri/zsh-async"
+$zp_cmd "mafredri/zsh-async"
 #Plugin that provides zsnapshot command which dumps current ZSH state into a file, for restoration by sourcing the file
-zplugin load "psprint/zsnapshot"
+$zp_cmd "psprint/zsnapshot"
 #A shell command that will display selection list
-zplugin load "psprint/zsh-select"
+$zp_cmd "psprint/zsh-select"
 #Curses-based tools for Zsh, e.g. multi-word history searcher
-zplugin load "psprint/zsh-navigation-tools"
+$zp_cmd "psprint/zsh-navigation-tools"
 #Complete options from manual pages – press Ctrl-F to start the completer
-zplugin load "psprint/zzcomplete"
+$zp_cmd "psprint/zzcomplete"
 #Record output of commands, reuse it via curses interface
-zplugin load "psprint/ztrace"
+$zp_cmd "psprint/ztrace"
 
 # A script to make using 256 colors in zsh less painful.
 zplugin snippet 'https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/spectrum.zsh'
 
 
 #zsh-autoenv automatically sources (known/whitelisted) .autoenv.zsh files, typically used in project root directories.
-zplugin load "Tarrasch/zsh-autoenv" # zsh-autoenv automatically sources (known/whitelisted) .autoenv.zsh files, typically used in project root directories.
+$zp_cmd "Tarrasch/zsh-autoenv" # zsh-autoenv automatically sources (known/whitelisted) .autoenv.zsh files, typically used in project root directories.
 
 #Command-line productivity booster, offers quick access to files and directories, inspired by autojump, z and v
 zplugin snippet 'https://github.com/skwp/dotfiles/blob/master/bin/fasd'
 
 source $HOME/.powerlevel9k
-#zplugin load "iladin/oh-my-git"
+#$zp_cmd "iladin/oh-my-git"
 #zplugin snippet 'https://github.com/arialdomartini/oh-my-git-themes/blob/master/oppa-lana-style.zsh-theme'
 #test -d $HOME/.git-radar || git clone https://github.com/michaeldfallen/git-radar $HOME/.git-radar;
 #test -d $HOME/.git-radar && pathadd $HOME/.git-radar && export PROMPT="$PROMPT\$(git-radar --zsh --fetch) "
@@ -95,16 +95,16 @@ source $HOME/.powerlevel9k
 zplugin snippet "https://github.com/skwp/dotfiles/blob/master/zsh/last-command.zsh"
 
 # zsh anything.el-like widget.
-zplugin load "zsh-users/zaw"
+$zp_cmd "zsh-users/zaw"
 # ZSH port of Fish shell's history search feature
-zplugin load "zsh-users/zsh-history-substring-search" #, nice:10
+$zp_cmd "zsh-users/zsh-history-substring-search" #, nice:10
 # Additional completion definitions for Zsh
-zplugin load "zsh-users/zsh-completions"
+$zp_cmd "zsh-users/zsh-completions"
 # Fish-like fast/unobtrusive autosuggestions for zsh.
-zplugin load "zsh-users/zsh-autosuggestions"
+$zp_cmd "zsh-users/zsh-autosuggestions"
 # Syntax highlighting bundle. zsh-syntax-highlighting must be loaded after
 # excuting compinit command and sourcing other plugins.
-zplugin load "zsh-users/zsh-syntax-highlighting" #, nice:9
+$zp_cmd "zsh-users/zsh-syntax-highlighting" #, nice:9
 
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 
