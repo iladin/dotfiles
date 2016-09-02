7c57a31a38a715b28660875a4be00b55c8cf8d25
Daniel Silverman <iladin@gmail.com>
2016-09-01 20:57:51 -0700

    Working towards compiling emacs with xwidgets

--- bin/makeEmacs.sh
+++ bin/makeEmacs.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 
+set -eu
+
+
+function xwidgets(){}
+sudo apt-get -qq update
+sudo apt-get -qq install -y stow build-essential libx11-dev xaw3dg-dev \
+     libjpeg-dev libpng12-dev libgif-dev libtiff5-dev libncurses5-dev \
+     libxft-dev librsvg2-dev libmagickcore-dev libmagick++-dev \
+     libxml2-dev libgpm-dev libotf-dev libm17n-dev \
+     libgnutls-dev wget make gcc-core gcc-g++ autoconf automake \
+     perl libpoppler-devel libpoppler-glib-devel libgtk-3-dev
+git clone -b emacs-25 git://git.sv.gnu.org/emacs.git && cd emacs
+./autogen.sh && ./configure --with-xwidgets && make
+}
+
+function easySnapshot(){
+    sudo add-apt-repository -y ppa:ubuntu-elisp
+    sudo apt-get update
+    sudo apt-get install emacs-snapshot
+}
+easySnapshot
+
+
 # Build latest version of Emacs, version management with stow
 # OS: Ubuntu 14.04 LTS and newer
 # version: 24.5
 # Toolkit: lucid
 
-set -eu
-
-function easySnapshot(){
-sudo add-apt-repository -y ppa:ubuntu-elisp
-sudo apt-get update
-sudo apt-get install emacs-snapshot
-}
-easySnapshot
 function old(){
 readonly version="24.5"
 
@@ -23,7 +38,7 @@ sudo apt-get -qq install -y stow build-essential libx11-dev xaw3dg-dev \
      libxft-dev librsvg2-dev libmagickcore-dev libmagick++-dev \
      libxml2-dev libgpm-dev libotf-dev libm17n-dev \
      libgnutls-dev wget make gcc-core gcc-g++ autoconf automake \
-     perl libpoppler-devel libpoppler-glib-devel
+     perl libpoppler-devel libpoppler-glib-devel libgtk-3-dev
 cd $TMPDIR
 
 # download source package
