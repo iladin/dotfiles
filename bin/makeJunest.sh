81657ebbab8325ff2a9b20a9fc513fccaa35a222
Daniel Silverman <iladin@gmail.com>
2016-09-02 17:13:54 -0700

    Use https instead of git protocol.

--- bin/makeJunest.sh
+++ bin/makeJunest.sh
@@ -1,3 +1,3 @@
-git clone git://github.com/fsquillace/junest ~/junest
+git clone https://github.com/fsquillace/junest ~/junest
 export PATH=~/junest/bin:$PATH
 junest -f -- pacman --noconfirm -Sy zsh awk pkgfile grep
