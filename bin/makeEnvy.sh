e34f778fe68d4434921e02891eae296d6327ad81
Daniel Silverman <iladin@gmail.com>
2016-09-28 17:01:10 -0700

    Replaced if with elif

--- bin/makeEnvy.sh
+++ bin/makeEnvy.sh
@@ -2,7 +2,7 @@
 function whereMyDockerfileAt(){
     if [[ -e codenvy.dockerfile ]]; then
         dockerfile=.codenvy.dockerfile
-    if [[ -e .codenvy.dockerfile ]]; then
+    elif [[ -e .codenvy.dockerfile ]]; then
         dockerfile=.codenvy.dockerfile
     elif [[ -e ~/.codenvy.dockerfile ]]; then
         dockerfile=~/.codenvy.dockerfile
