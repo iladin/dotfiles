b0f366279909d5dcfed30fae049f7ffc32470cbb
Daniel Silverman <iladin@gmail.com>
2016-10-27 00:13:41 -0700

    Added Man function
    This sets the color inside the function.
    This means it won't clog your ENV
    set & printenv are back on the menu

--- .functions.sh
+++ .functions.sh
@@ -129,6 +129,17 @@ function flagCheckout(){
     fi
 }
 
+man() {
+    LESS_TERMCAP_mb=$'\e[1;36m' \
+    LESS_TERMCAP_md=$'\e[1;36m' \
+    LESS_TERMCAP_me=$'\e[0m' \
+    LESS_TERMCAP_se=$'\e[0m' \
+    LESS_TERMCAP_so=$'\e[1;44;33m' \
+    LESS_TERMCAP_ue=$'\e[0m' \
+    LESS_TERMCAP_us=$'\e[1;32m' \
+    command man "$@"
+}
+
 # git functions: {{{
 # Outputs current branch info in prompt format
 function git_prompt_info() {
