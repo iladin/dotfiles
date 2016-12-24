d928b060e9c0942a2db284ef70844f96f8f2fbde
iladin <iladin@gmail.com>
2016-12-24 14:12:48 -0800

    Added zplug entries from zplug's creator

--- .zplug.zsh
+++ .zplug.zsh
@@ -7,7 +7,30 @@ zzplug
 zplug "k4rthik/git-cal", as:command
 zplug "mchav/with", as:command
 zplug "mrowa44/emojify",  as:command
-zplug "awsonbotsford/swim",  as:command
+zplug "dawsonbotsford/swim",  as:command
+
+zplug "monochromegane/the_platinum_searcher", \
+    as:command, \
+    from:gh-r, \
+    rename-to:"pt", \
+    frozen:1
+
+zplug "peco/peco", \
+    as:command, \
+    from:gh-r, \
+    frozen:1
+
+zplug "b4b4r07/peco-tmux.sh", \
+    as:command, \
+    use:'*.sh', \
+    rename-to:'peco-tmux'
+
+zplug "philovivero/distribution", \
+    as:command, \
+    use:distribution, \
+    if:'(( $+commands[perl] ))'
+
+
 
 function zplug_end() {
     if ! zplug check --verbose; then
