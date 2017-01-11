c9fee830ba840e2fda07c323b1c18e65cac56768
iladin <iladin@gmail.com>
2017-01-11 08:30:42 +0000

    Use bdb instead of vbisam

--- bin/makeGnuCobol.sh
+++ bin/makeGnuCobol.sh
@@ -1,29 +1,30 @@
 #!/usr/bin/env bash
 #TODO: ERROR CHECKING JEEZ
 sudo apt-get update
-sudo apt-get -yqq install help2man texinfo
+sudo apt-get -yqq install help2man texinfo libdb5.3-dev flex libgmp3-dev libncurses5-dev
 mkdir ~/tmp 2> /dev/null
 cd ~/tmp
-rm -rf opensource-cobol
-git clone https://github.com/opensourcecobol/opensource-cobol.git
-cd opensource-cobol/vbisam
-chmod a+x configure
-./configure
-make cycle
-sudo make install
-sudo ldconfig
+#rm -rf opensource-cobol
+#git clone https://github.com/opensourcecobol/opensource-cobol.git
+#cd opensource-cobol/vbisam
+#chmod a+x configure
+#./configure
+#make cycle
+#sudo make install
+#sudo ldconfig
 cd ~/tmp
 test -d gnucobol && rm -rf gnucobol
 git clone https://gitlab.com/gnu/cobol.git
 cd cobol
-./configure --with-vbisam
+#./configure --with-vbisam
+./configure --with-db
 make || exit 1
 make check
 sudo make install || exit 1
 sudo ldconfig
 cd ~/tmp
-git svn clone svn://svn.code.sf.net/p/open-cobol/contrib/ open-cobol-contrib
+test -d open-cobol-contrib && rm -rf open-cobol-contrib
+git clone https://gitlab.com/iladin/open-cobol-contrib.git
 cd open-cobol-contrib*/trunk/tools/cobolmac
-sudo ldconfig
 source comp-cobolmac.sh
 sudo cp cobolmac /usr/bin
