0bfd597beb36f4de5334c3f3f6eadfe370608762
iladin <iladin@gmail.com>
2017-03-21 00:45:54 -0700

    missed another sudo
    bye bye sleep

--- bin/makeVim.sh
+++ bin/makeVim.sh
@@ -1,4 +1,4 @@
- apt update && sudo apt install libncurses5-dev libgnome2-dev libgnomeui-dev \
+ apt update && apt install libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev tcl-dev libx11-dev libxpm-dev libxt-dev python-dev \
     python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git libluajit-5.1-dev
