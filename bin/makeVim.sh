d6406878d6265b5021c23a28af6bbc58b34fe561
iladin <iladin@gmail.com>
2017-03-22 22:08:00 -0700

    Try with lua /usr/local prefix

--- bin/makeVim.sh
+++ bin/makeVim.sh
@@ -3,7 +3,7 @@
     libcairo2-dev tcl-dev libx11-dev libxpm-dev libxt-dev python-dev \
     python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git libluajit-5.1-dev
 
-cp /usr/include/lua5.1/*  /usr/include/lua5.1/include
+
 git clone --depth 1 https://github.com/vim/vim.git
 
 cd vim
@@ -19,7 +19,7 @@ cd vim
 --enable-python3interp \
 --enable-luainterp \
 --with-luajit \
---with-lua-prefix=/usr/include/lua5.1 \
+--with-lua-prefix=/usr/local \
 --enable-gpm \
 --enable-cscope \
 --enable-fontset \
