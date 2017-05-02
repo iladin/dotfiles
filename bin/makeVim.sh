be9c82ac5217456b3392cbd490d52c8918bcd239
iladin <iladin@gmail.com>
2017-05-01 22:49:45 -0700

    end with make install

--- bin/makeVim.sh
+++ bin/makeVim.sh
@@ -27,8 +27,3 @@ cd vim
 
 
  make install
-
- update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
- update-alternatives --set editor /usr/bin/vim
- update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
- update-alternatives --set vi /usr/bin/vim
