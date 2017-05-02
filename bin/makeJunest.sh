1523544fb6b386f22d1767203e7ed50c0b37c16e
iladin <iladin@gmail.com>
2017-05-02 01:07:00 -0700

    I am groot

--- bin/makeJunest.sh
+++ bin/makeJunest.sh
@@ -1,3 +1,8 @@
 git clone https://github.com/fsquillace/junest ~/junest
 export PATH=~/junest/bin:$PATH
-junest -f -- pacman --noconfirm -Sy zsh awk pkgfile grep
+if [[ $(id -u) -ne 0 ]]; then
+    junest -f -- pacman --noconfirm -Sy zsh awk pkgfile grep git
+else
+    junest -f --groot  -- pacman --noconfirm -Sy zsh awk pkgfile grep git
+fi
+
