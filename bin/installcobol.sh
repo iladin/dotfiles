86675f2700f1a61af4f01a0f1399b4ea00214a49
iladin <iladin@gmail.com>
2022-12-30 18:16:20 +0000

    fix installcobol.sh script

--- bin/installcobol.sh
+++ bin/installcobol.sh
@@ -2,11 +2,17 @@
 set -xe
 cob_version=${1:-3.1.2}
 cob_url=https://versaweb.dl.sourceforge.net/project/gnucobol/gnucobol # Sourceforge changes this
-curl_flags="--silent --show-error--location --insecure"
-sudo apt install build-essential autoconf libgmp-dev libdb-dev
+curl_flags="--show-error --location --insecure"
+if hash apt; then
+  sudo apt update
+  sudo apt install -y build-essential autoconf libgmp-dev libdb-dev
+elif hash yum; then
+  sudo yum makecache
+  sudo yum install gmp-devel.x86_64 libdb-devel.x86_64
+fi
 sudo ldconfig # Runs so configure can see gcc
 cd "${TMPDIR:=/tmp}"
-curl "${curl_flags}" "${cob_url}/${cob_version%.*}/gnucobol-${cob_version}.tar.gz"
+curl ${curl_flags} "${cob_url}/${cob_version%.*}/gnucobol-${cob_version}.tar.gz" --output gnucobol-"${cob_version}".tar.gz 
 tar xfv gnucobol-"${cob_version}".tar.gz
 cd gnucobol-"$cob_version"
 ./configure --prefix=/
